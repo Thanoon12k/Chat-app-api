7a094d881c580bed355d718b0ae1d145a0c73598
Thanoon <engthanoon1@gmail.com>
2023-01-24 23:43:50 +0300

    edit routing

--- asgiapp/urls.py
+++ asgiapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("chat", views.index, name="index"),
-    path("sync_rooms/<str:room_name>/", views.room, name="room"),
+    path("ws/rooms/<str:room_name>/messages/", views.room, name="room"),
     path("async_rooms/<str:room_name>/", views.room, name="room"),
     path("rest_rooms/<str:room_name>/", views.room, name="room"),
 ]
