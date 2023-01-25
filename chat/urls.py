53a30fa416f2322a6ca63ccaf59baa16b942dd77
Thanoon <engthanoon1@gmail.com>
2023-01-25 14:08:36 +0300

    add staticroot to urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -2,12 +2,18 @@
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', include('asgiapp.urls')),
     
   
-   
+
     
 ]
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+           
